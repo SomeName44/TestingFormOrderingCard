bdc7f41471b8f343d13a77bde0d825a035946e28
Olga <deputat.d@yandex.com>
2023-04-07 15:40:23 +0300

    add selenium test

---
+++ src/test/java/ru/netology/SeleniumTest.java
@@ -0,0 +1,51 @@
+package ru.netology;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SeleniumTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        //WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver2.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() {
+        driver.get("http://localhost:7777");
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василюк Ольга");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79251809626");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        assertEquals(expected, actual);
+    }
+}
